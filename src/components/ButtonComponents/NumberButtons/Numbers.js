b3f0875b43da0ec81b1cb8b80f9f9606ba14c430
Alex Martin <atonymartin20@yahoo.com>
2019-08-16 21:59:01 -0500

    Numbers should work when implemented.

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import NumberButton from './NumberButton.js';
 import { numbers } from '../../../data.js';
-
 //import any components needed
+
 
 //Import your array data to from the provided data file
 
-const Numbers = () => {
+const Numbers = (props) => {
   // STEP 2 - add the imported data to state
+  const [numberState, setNumberState] = useState(numbers);
   return (
     <div>
+      {numberState.map(number => {
+        return (
+          <NumberButton key={number} text={number} addNumber={props.addNumber} />
+        )
+      })}
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
