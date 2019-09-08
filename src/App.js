4fe5ee4a016492c8bb096f3a8b66a4541323f8c6
Axis <atonymartin20@yahoo.com>
2019-09-07 22:27:47 -0500

    Special buttons check

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
       <Logo />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
-        {/* Commit 3 */}
+        {/* Commit 4 */}
         <Display number={displayValue} />
         <Numbers addNumber={addNumber} />
         <Specials />
