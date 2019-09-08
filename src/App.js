48c05dc448417f5db5cad4b403c93929b1606e7e
Axis <atonymartin20@yahoo.com>
2019-09-07 22:27:32 -0500

    Display check

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
       <Logo />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
-        {/* Commit 2 */}
+        {/* Commit 3 */}
         <Display number={displayValue} />
         <Numbers addNumber={addNumber} />
         <Specials />
