c30fbe71bd93441f0d417707e1e7aed1faedc368
Axis <atonymartin20@yahoo.com>
2019-09-08 10:54:53 -0500

    Number Buttons styled.

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "./index.css";
 
 const NumberButton = (props) => {
   return (
     <>
       <button onClick={() => {
         props.addNumber(props.text)
-      }}>
+      }} className="number-button">
         {props.text}
       </button>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
